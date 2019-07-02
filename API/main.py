333e16297bef0896c10d7ced3346ab2fa01c1b7a
lishizalice <lishi.alice@hotmail.fr>
2019-07-02 13:41:16 +0000

    Add user authentication

--- API/main.py
+++ API/main.py
@@ -167,4 +167,5 @@ def delete_transaction(id):
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', port=8155, debug=True)
--- app/server.py
+++ app/server.py
@@ -2,14 +2,32 @@
 
 from functools import wraps
 from flask import Flask, render_template, request, redirect, flash, url_for
-from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from queries import *
+from models import *
 from config import Config
+from ldap_authenticate import authenticate
 
-app = Flask(__name__)
-app.config.from_object(Config)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(ldap_user_id):
+    return LdapUser.query.get(int(ldap_user_id))
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/")
+@login_required
 def default():
     return render_template('index.html', events=get_events(), 
                           users=get_users(), total=get_total_paid(), 
@@ -19,23 +37,27 @@ def default():
 
 
 @app.route("/pay")
+@login_required
 def remboursement():
     return render_template('pay.html')
 
 
 @app.route("/event/<id>")
+@login_required
 def event(id):
     event = get_event_with_username(id)
     return render_template('event.html', event=event)
 
 
 @app.route("/user/<id>")
+@login_required
 def user(id):
     user = get_user_with_eventname(id)
     return render_template('user.html', user=user)
 
 
 @app.route("/update_transaction/<referrer>/<id_referrer>/<id>")
+@login_required
 def update_transaction(referrer, id_referrer, id):
     if referrer in ['user', 'event']:
         update_transaction_status(id)
@@ -44,11 +66,38 @@ def update_transaction(referrer, id_referrer, id):
         abort(404)
 
 
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    login = request.form['inputLogin']
+    password = request.form['inputPassword']
+    if authenticate(login, password):
+        ldap_user = LdapUser.query.filter_by(login=login).first()
+        if ldap_user is None:
+            ldap_user = LdapUser(login=login, is_admin=False)
+            db.session.add(ldap_user)
+            db.session.commit()
+            ldap_user = LdapUser.query.filter_by(login=login).first()
+        flash('Authentification effectuée avec succès.', 'success')
+        login_user(ldap_user)
+        return redirect('/')
+    else: 
+        flash('Echec de l\'authentification.', 'error')
+        return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect('/login')
+
 @app.errorhandler(404)
-def erreur404(error): 
+def error404(error): 
     flash(str(error), 'error')
     return redirect(url_for('default'))
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', port=8154, debug=True)
